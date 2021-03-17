98672799b420ee3401a487b8ffe6f32b8915d4bb
Hp <nirajbhatta108@gmail.com>
2021-03-17 10:04:36 +0545

    Done

---
+++ LabExercise/compute_the _ area.py
@@ -0,0 +1,2 @@
+# write the Python program whicj accepts the radius of a circle from the user and compute the area.
+# area of circle = PI * r^2
---
+++ LabExercise/quicker_or_slower.py
@@ -0,0 +1,4 @@
+# you live 4 miles from university. The bus drives at 25mph but spends 2 min at each of the 10 stops on the way.
+# How long will the bus journey take? Alternatively you could run to the university. you jog the first mile at 7pm;
+# then run thw next two at 15mph; before jogging the last at 7mph again. Will this be quicker or slower than the bus?
+
